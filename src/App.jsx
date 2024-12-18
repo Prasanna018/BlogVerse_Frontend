19143b6dce6d24905259d3ea30512a8a60ed004d
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2024-12-18 20:23:51 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Login from './authPages/Login';
@@ -12,22 +13,45 @@ import UserCardList from './components/UserPosts/UserCardList';
 import ProfilePage from './Pages/ProfilePage/ProfilePage';
 import MainProfilePage from './Pages/ProfilePage/MainProfilePage';
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage></HomePage>
+
+  },
+  {
+    path: '/login',
+    element: <Login></Login>
+  },
+  {
+    path: '/signup',
+    element: <SignUp></SignUp>
+  },
+  {
+    path: '/create-post',
+    element: <CreatePost></CreatePost>
+  },
+  {
+    path: '/post/:id',
+    element: <PostView></PostView>
+  }, {
+    path: '/users-posts',
+    element: <UserCardList></UserCardList>
+
+  },
+  {
+    path: '/user-profile',
+    element: <MainProfilePage></MainProfilePage>
+  }
+
+])
 // import CardList from './components/Post/CardList';
 function App() {
   return (
-    <Router>
-      <Toaster></Toaster>
-      <Routes>
-        <Route path='/' element={<HomePage></HomePage>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<SignUp></SignUp>}></Route>
-        <Route path='/create-post' element={<CreatePost></CreatePost>}></Route>
-        <Route path="/post/:id" element={<PostView></PostView>} />
-        <Route path='/users-posts' element={<UserCardList></UserCardList>}></Route>
-        <Route path='/user-profile' element={<MainProfilePage></MainProfilePage>}></Route>
-      </Routes>
+    <RouterProvider router={appRouter}>
 
-    </Router>
+    </RouterProvider>
+
   );
 }
 
